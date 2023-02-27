5ff72d849fe0972e94502df2fa7f011916ddbe4a
Charlie Norin <chanor101@gmail.com>
2023-02-27 23:49:49 +0100

    Reordering and fixing shaders

--- example/assets/shaders/st0.frag
+++ example/assets/shaders/st0.frag
@@ -107,8 +107,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     vec2 uv = fragCoord/iResolution.x;
 	float shade = pattern(uv);
-    // fragColor = vec4(colormap(shade).rgb, shade);
-    fragColor = vec4(1.);
+    fragColor = vec4(colormap(shade).rgb, shade);
 }
 
 ////////////// Shadertoy END
--- example/lib/gallery/gallery_shaders.dart
+++ example/lib/gallery/gallery_shaders.dart
@@ -2,16 +2,16 @@ import 'package:shady/shady.dart';
 
 final galleryShaders = [
   // ShaderToy shaders
-  Shady(assetName: 'assets/shaders/st0.frag', shaderToy: true),
-  Shady(assetName: 'assets/shaders/st1.frag', shaderToy: true),
-  Shady(assetName: 'assets/shaders/st2.frag', shaderToy: true),
   Shady(assetName: 'assets/shaders/st3.frag', shaderToy: true),
+  Shady(assetName: 'assets/shaders/st0.frag', shaderToy: true),
+  Shady(assetName: 'assets/shaders/st2.frag', shaderToy: true),
   Shady(assetName: 'assets/shaders/st4.frag', shaderToy: true),
-  Shady(assetName: 'assets/shaders/st5.frag', shaderToy: true),
   Shady(assetName: 'assets/shaders/st6.frag', shaderToy: true),
   Shady(assetName: 'assets/shaders/st7.frag', shaderToy: true),
   Shady(assetName: 'assets/shaders/st8.frag', shaderToy: true),
   Shady(assetName: 'assets/shaders/st9.frag', shaderToy: true),
+  Shady(assetName: 'assets/shaders/st5.frag', shaderToy: true),
+  Shady(assetName: 'assets/shaders/st1.frag', shaderToy: true),
 
   // Image shaders
   Shady(
